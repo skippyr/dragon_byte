55b7582a6682cc6a2703356e516393ced299c3df
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-06 18:11:55 -0300

    refactor: turn attributes of X11Cursor private

--- scripts/x11_cursor_installer.py
+++ scripts/x11_cursor_installer.py
@@ -2,50 +2,51 @@ import os
 
 class PathUtilities:
 	@staticmethod
-	def get_repository_directory():
+	def get_repository_directory_path():
 		return os.path.dirname(os.path.dirname(__file__))
 
 class X11Cursor:
 	def __init__(
 		self,
 		name,
-		output_directory,
-		settings_directory,
+		output_directory_path,
+		settings_directory_path,
 		symlinks
 	):
-		self.name = name
-		self.output_directory = output_directory
-		self.settings_directory = settings_directory
-		self.symlinks = symlinks
+		self.__name = name
+		self.__output_directory_path = output_directory_path
+		self.__settings_directory_path = settings_directory_path
+		self.__symlinks = symlinks
 	
 	def get_name(self):
-		return self.name
+		return self.__name
 	
-	def get_output_directory(self):
-		return self.output_directory
+	def get_output_directory_path(self):
+		return self.__output_directory_path
 	
-	def get_settings_directory(self):
-		return self.settings_directory
+	def get_settings_directory_path(self):
+		return self.__settings_directory_path
 	
 	def get_symlinks(self):
-		return self.symlinks
+		return self.__symlinks
 
 class X11CursorInstaller:
 	@staticmethod
 	def main():
 		cursor = X11Cursor(
 			name = "Dragon Byte",
-			output_directory = os.path.join(
-				PathUtilities.get_repository_directory(),
+			output_directory_path = os.path.join(
+				PathUtilities.get_repository_directory_path(),
 				"dragon_byte"
 			),
-			settings_directory = os.path.join(
-				PathUtilities.get_repository_directory(),
+			settings_directory_path = os.path.join(
+				PathUtilities.get_repository_directory_path(),
 				"settings"
 			),
 			symlinks = []
 		)
-		print(cursor.get_output_directory())
+		print(cursor.get_settings_directory_path())
+		print(cursor.get_output_directory_path())
 
 X11CursorInstaller.main()
 
