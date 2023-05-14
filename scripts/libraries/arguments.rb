55ab1ea6bd6f03489c2101fa5650ee040e3d6576
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-13 22:41:38 -0300

    feat: add arguments parsing

---
+++ scripts/libraries/arguments.rb
@@ -0,0 +1,14 @@
+class Arguments
+	def self.is_to_print_help_instructions?()
+		ARGV.include?("--help")
+	end
+
+	def self.is_to_create_web_port?()
+		ARGV.include?("create-web-port")
+	end
+
+	def self.is_to_create_x11_port?()
+		ARGV.include?("create-x11-port")
+	end
+end
+
